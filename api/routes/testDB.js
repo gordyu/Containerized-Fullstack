961a98f4af8a56cd3431199af2d3b333e279093b
Gordon Yu <gyunit@gmail.com>
2019-09-01 16:27:20 -0700

    resolved all security vulnerabilities and local instance passing all tests

--- api/routes/testDB.js
+++ api/routes/testDB.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res, next) {
 
 // Connecting to MongoDB
 mongoose.connect(
-    "mongodb://mongo-database:27017/test",
+    "mongodb://localhost:27017/test",
     { useNewUrlParser: true }
 ).then(() => console.log('MONGOOSE CONNECTED')
 ).catch(err => console.log('MONGOOSE FAILED TO CONNECT'));
